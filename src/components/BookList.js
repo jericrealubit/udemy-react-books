6a10e817dcb7087b0e9749b03f564e22187fcf1b
jeric realubit <jericrealubit@gmail.com>
2023-04-06 23:35:53 +1200

    useContext done

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -2,16 +2,14 @@ import { useContext } from "react";
 import BooksContext from "../contexts/Books";
 import BookShow from "./BookShow";
 
-function BookList({ books, onDelete, onEdit }) {
-  //const { count, incrementCount } = useContext(BooksContext);
+function BookList() {
+  const { books } = useContext(BooksContext);
 
   const renderedBooks = books.map((book) => {
-    return <BookShow onEdit={onEdit} onDelete={onDelete} key={book.id} book={book} />
+    return <BookShow key={book.id} book={book} />
   });
 
   return <div className="book-list">
-    {/* {count}
-    <button onClick={incrementCount}>Click</button> */}
     {renderedBooks}
   </div>;
 }
